a8267f37897830fdbc63f3999f21d624e80f18f7
sageb18 <taddeosage@gmail.com>
2026-08-28 21:15:40 -0700

    initial commit

---
+++ src/main/java/com/sageb18/distributedorderinventory/DistributedOrderInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.sageb18.distributedorderinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedOrderInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedOrderInventoryApplication.class, args);
+	}
+
+}
