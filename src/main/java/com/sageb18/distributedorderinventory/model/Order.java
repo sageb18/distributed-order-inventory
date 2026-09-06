145ebdb19de8fcbc37d4171a2f4770dcd5a29f52
sageb18 <taddeosage@gmail.com>
2026-09-05 18:10:13 -0700

    completed order model

--- src/main/java/com/sageb18/distributedorderinventory/model/Order.java
+++ src/main/java/com/sageb18/distributedorderinventory/model/Order.java
@@ -1,4 +1,56 @@
 package com.sageb18.distributedorderinventory.model;
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
 public class Order {
+
+    private String orderId;
+    private String productId;
+    private int quantity;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(String orderId, String productId, int quantity, OrderStatus status) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    @DynamoDbPartitionKey
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
