bde2da617d93831cdde78cbf33f9d9f116c55ba5
sageb18 <taddeosage@gmail.com>
2026-09-05 18:19:30 -0700

    finished orderservice: createOrder, getOrderById, getAllOrders

--- src/main/java/com/sageb18/distributedorderinventory/model/OrderStatus.java
+++ src/main/java/com/sageb18/distributedorderinventory/model/OrderStatus.java
@@ -3,5 +3,6 @@ package com.sageb18.distributedorderinventory.model;
 public enum OrderStatus {
     PENDING,
     CONFIRMED,
-    OUT_OF_STOCK
+    OUT_OF_STOCK,
+    FAILED
 }
---
+++ src/main/java/com/sageb18/distributedorderinventory/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.sageb18.distributedorderinventory.service;
+
+import com.sageb18.distributedorderinventory.model.Order;
+import com.sageb18.distributedorderinventory.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order) {
+        order.setOrderId(UUID.randomUUID().toString());
+
+        return orderRepository.save(order);
+    }
+
+    public Order getOrderById(String orderId) {
+        return orderRepository.findById(orderId);
+    }
+
+    public Collection<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
